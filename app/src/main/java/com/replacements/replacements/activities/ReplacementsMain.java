ed0f55d3ce3fab1db7a295eba69f81d4221d7f13
o_r_z_e_c_h <tarasiukdawid@gmail.com>
2017-02-20 18:00:40 +0100

    Handling app indexing Google Search (open links in app) and add Firebase Analitics (checking number of clicking on these links)

--- app/src/main/java/com/replacements/replacements/activities/ReplacementsMain.java
+++ app/src/main/java/com/replacements/replacements/activities/ReplacementsMain.java
@@ -10,6 +10,7 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
@@ -34,6 +35,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 //import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.replacements.replacements.data.ClassDbAdapter;
 import com.replacements.replacements.data.DbAdapter;
@@ -58,6 +60,7 @@ import com.replacements.replacements.sync.ScheduleUpdate;
 
 public class ReplacementsMain extends AppCompatActivity {
     private static final String CLASS_NAME = ReplacementsMain.class.getName();
+    private FirebaseAnalytics mFirebaseAnalytics;
     public ConnectivityManager connManager;
     private NavigationView mNavigationView;
     private int mCurrentSelectedPosition;
@@ -87,7 +90,6 @@ public class ReplacementsMain extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
         Log.i(CLASS_NAME, "onCreate 100");
 
@@ -158,43 +160,68 @@ public class ReplacementsMain extends AppCompatActivity {
             boolean isNotified;
 
 
-            if (savedInstanceState != null) {
-                Log.i("QQQQQQQQQ", "4");
-                mCurrentSelectedPosition = savedInstanceState.getInt("STATE_SELECTED_POSITION");
+
+            Intent intent = getIntent();
+            String action = intent.getAction();
+            final Uri data = intent.getData();
+            //Check whether activity was instantiated from App Indexing Google Search and open appropriate content
+            if (Intent.ACTION_VIEW.equals(action) && data != null) {
+                int mapWebMenuItem = mapWebLink(data);
+                Log.i(CLASS_NAME, "onCreate from internet 100");
+                //Firebase analytics (send just once, therefore "savedInstanceState == null")
+                if (savedInstanceState == null) {
+                    Log.i(CLASS_NAME, "onCreate from internet 200");
+                    mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+                    Bundle params = new Bundle();
+                    params.putString("open_from_internet", (mapWebMenuItem == 2) ? "zastepstwa" : "plan");
+                    mFirebaseAnalytics.logEvent("open_from_internet_event", params);
+                }
+                Log.i(CLASS_NAME, "onCreate from internet 300");
                 Menu menu = mNavigationView.getMenu();
-                menu.getItem(mCurrentSelectedPosition).setChecked(true);
-                menuItems(menu.getItem(mCurrentSelectedPosition));
-            } else if (intentExtra != null) {
-                Log.i("QQQQQQQQQ", "1");
-                isNotified = intentExtra.getBoolean("isNotified", false);
-                Log.i("resume", "2");
-                if (isNotified) {
-                    Log.i("resume", "3");
-                    int extraMenuItem = intentExtra.getInt("menuItem", 2);
+                menu.getItem(mapWebMenuItem).setChecked(true);
+                menuItems(menu.getItem(mapWebMenuItem));
+            }else{
+                Log.i(CLASS_NAME, "onCreate from Android 100");
+                if (savedInstanceState != null) {
+                    Log.i(CLASS_NAME, "onCreate from Android 200");
+                    mCurrentSelectedPosition = savedInstanceState.getInt("STATE_SELECTED_POSITION");
                     Menu menu = mNavigationView.getMenu();
-                    menu.getItem(extraMenuItem).setChecked(true);
-                    menuItems(menu.getItem(extraMenuItem));
-                    if (extraMenuItem == 2) {
-                        SharedPreferences mSharedPreferences = getSharedPreferences("dane", 0);
-                        SharedPreferences.Editor mLocalEditor = mSharedPreferences.edit();
-                        mLocalEditor.putLong("savedTime", 0);
-                        mLocalEditor.apply();
+                    menu.getItem(mCurrentSelectedPosition).setChecked(true);
+                    menuItems(menu.getItem(mCurrentSelectedPosition));
+                } else if (intentExtra != null) {
+                    Log.i(CLASS_NAME, "onCreate from Android 300");
+                    isNotified = intentExtra.getBoolean("isNotified", false);
+                    if (isNotified) {
+                        Log.i(CLASS_NAME, "onCreate from Android 400");
+                        int extraMenuItem = intentExtra.getInt("menuItem", 2);
+                        Menu menu = mNavigationView.getMenu();
+                        menu.getItem(extraMenuItem).setChecked(true);
+                        menuItems(menu.getItem(extraMenuItem));
+                        if (extraMenuItem == 2) {
+                            Log.i(CLASS_NAME, "onCreate from Android 500");
+                            SharedPreferences mSharedPreferences = getSharedPreferences("dane", 0);
+                            SharedPreferences.Editor mLocalEditor = mSharedPreferences.edit();
+                            mLocalEditor.putLong("savedTime", 0);
+                            mLocalEditor.apply();
+                        }
+                    } else {
+                        Log.i(CLASS_NAME, "onCreate from Android 600");
+                        Menu menu = mNavigationView.getMenu();
+                        menu.getItem(2).setChecked(true);
+                        menuItems(menu.getItem(2));
                     }
                 } else {
-                    Log.i("QQQQQQQQQ", "5-1");
+                    Log.i(CLASS_NAME, "onCreate from Android 700");
                     Menu menu = mNavigationView.getMenu();
                     menu.getItem(2).setChecked(true);
                     menuItems(menu.getItem(2));
                 }
-            } else {
-                Log.i("QQQQQQQQQ", "5-2");
-                Menu menu = mNavigationView.getMenu();
-                menu.getItem(2).setChecked(true);
-                menuItems(menu.getItem(2));
+                Log.i(CLASS_NAME, "onCreate from Android 800");
             }
 
         }
 
+        Log.i(CLASS_NAME, "onCreate 300");
 
 //        connManager = ((ConnectivityManager)getBaseContext().getSystemService(Context.CONNECTIVITY_SERVICE));
 //        //Sprawdzenie czy w ustawieniach sa wlaczone powiadomienia GCM
@@ -233,6 +260,22 @@ public class ReplacementsMain extends AppCompatActivity {
 //                }
 //            }
 //        }
+    }
+
+    private int mapWebLink(Uri uri) {
+        final String path = uri.getPath();
+        Log.i(CLASS_NAME, "mapWebLink uri path: " + path);
+        String[] pathSplit = path.split("/");
+        Log.i(CLASS_NAME, "mapWebLink uri pathSplit: " + pathSplit[1]);
+        switch (pathSplit[1]) {
+            case "zastepstwa":
+                Log.i(CLASS_NAME, "mapWebLink 2");
+                return 2;
+            case "plan":
+                Log.i(CLASS_NAME, "mapWebLink 3");
+                return 3;
+        }
+        return 2;
     }
 
     // Check if Google Playservices is installed in Device or not
@@ -901,9 +944,25 @@ public class ReplacementsMain extends AppCompatActivity {
                     //((ScheduleFragment) fragmentWebView).setScaleWebView();
                 }else {
                     Log.i(CLASS_NAME, "onBackPressed not goback");
+                    Intent intent = getIntent();
+                    String action = intent.getAction();
+                    String data = intent.getDataString();
+                    //Check whether activity was instantiated from App Indexing Google Search and on back pressed close current activity task
+                    // without closing but not showing previous activity tasks remaining in memory
+                    if (Intent.ACTION_VIEW.equals(action) && data != null) {
+                        moveTaskToBack(true);
+                    }
                     super.onBackPressed();
                 }
             }else{
+                Intent intent = getIntent();
+                String action = intent.getAction();
+                String data = intent.getDataString();
+                //Check whether activity was instantiated from App Indexing Google Search and on back pressed close current activity task
+                // without closing but not showing previous activity tasks remaining in memory
+                if (Intent.ACTION_VIEW.equals(action) && data != null) {
+                    moveTaskToBack(true);
+                }
                 super.onBackPressed();
             }
         }
