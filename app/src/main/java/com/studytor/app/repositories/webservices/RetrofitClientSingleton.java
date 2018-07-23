0542a4222bbfbef85c07e2b9e2d54987b379c7e5
orzechdev <tarasiukdawid@gmail.com>
2018-07-23 20:04:43 +0200

    Temporary implementation of Basic HTTP Authentication

--- app/src/main/java/com/studytor/app/repositories/webservices/RetrofitClientSingleton.java
+++ app/src/main/java/com/studytor/app/repositories/webservices/RetrofitClientSingleton.java
@@ -1,5 +1,12 @@
 package com.studytor.app.repositories.webservices;
 
+import java.io.IOException;
+
+import okhttp3.Credentials;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -16,9 +23,34 @@ public class RetrofitClientSingleton {
     }
 
     private void buildRetrofit() {
+        // ####
+        // #### TEMPORARY beginning
+        // ####
+        // Basic HTTP Authentication
+        String username = "httpauth";
+        String password = "$tudy-|-0r20!8";
+        String authToken = Credentials.basic(username, password);
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request.Builder ongoing = chain.request().newBuilder();
+                        ongoing.addHeader("Accept", "application/json;versions=1");
+                        //if (isUserLoggedIn()) {
+                            ongoing.addHeader("Authorization", authToken);
+                        //}
+                        return chain.proceed(ongoing.build());
+                    }
+                })
+                .build();
+        // ####
+        // #### TEMPORARY end
+        // ####
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://studytor.com/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient)
                 .build();
 
         this.webService = retrofit.create(WebService.class);
